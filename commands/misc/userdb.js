bda7df58dbf47f1b447235b8a657146ffed28481
culinaryAmphibian <teperbudget@gmail.com>
2021-03-11 19:02:56 -0500

    epico

--- commands/misc/userdb.js
+++ commands/misc/userdb.js
@@ -23,7 +23,7 @@ module.exports =
             message.channel.send(`hey there, ${message.author.username}! I haven't seen you in ${ Math.floor((( new Date().getTime()) - (UserJSON[message.author.id].lastMessage))/(1000 * 60 * 60)) } hours!`);
         }
         UserJSON[message.author.id].lastMessage = new Date().getTime();
-        if (UserJSON[message.author.id].name !== message.author.tag)
+        if (!UserJSON[message.author.id].name.includes(message.author.tag))
         {
             UserJSON[message.author.id].name[ (UserJSON[message.author.id].name.length) ] = message.author.tag;
         }

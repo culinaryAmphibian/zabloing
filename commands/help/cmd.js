81ec40c30ec02d3b6172dffc48c212cd2d978cc8
culinaryAmphibian <teperbudget@gmail.com>
2021-06-14 15:45:12 -0400

    fix alias bug

--- commands/help/cmd.js
+++ commands/help/cmd.js
@@ -9,11 +9,26 @@ module.exports =
         let prefix = ServerJSON[message.guild.id].prefix || '.';
         let cmd = bot.commands.get(bot.commands.map(c => c.name).find(cmdNames => cmdNames.includes(args[1])));
         let embed = {color: global.blueCol, title: `${prefix}${cmd.name[0]}`, description: 'aliases: ', fields: [], footer: global.footer};
+
         let aliases = [];
-        cmd.name.length > 1 ? aliases.concat(cmd.name.slice(1)) : embed.description += 'none';
+        if (cmd.name.length > 1) aliases.concat(cmd.name.slice(1));
         let serverAlias = cmd.name.find(n => ServerJSON[message.guild.id]?.cmds?.find?.(c => c.name == (`[pref]${n}`)))
         if (serverAlias) aliases.push(serverAlias.name);
-        aliases ? embed.description = `aliases: ${aliases.slice(1).join(', ').concat(`and ${aliases.pop()}`)}` : embed.description = `aliases: none`;
+        switch (aliases.length)
+        {
+            case 0:
+                embed.description = 'aliases: none';
+                break;
+            case 1:
+                embed.description += aliases.pop();
+                break;
+            case 2:
+                embed.description += `${aliases[0]} and ${aliases[1]}`;
+                break;
+            default:
+                embed.description += `${aliases.slice(0, -1).join(', ')}, and ${aliases.pop()}`;
+                break;
+        }
         embed.fields.push({name: 'what does it do?', value: cmd.description.replace(/\[curr\]/gi, Config.currency).replace(/\[pref\]/gi, prefix)});
         let usageVal;
         cmd.usage ? usageVal = cmd.usage.replace(/\[pref\]/gi, prefix).replace(/\[curr\]/gi, Config.currency) : usageVal = `${prefix}${cmd.name[0]} (this command doesn't accept parameters)`;

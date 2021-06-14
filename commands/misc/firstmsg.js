83a6964fe0534dd0ec1043b1194323b1dd2dfc49
culinaryAmphibian <teperbudget@gmail.com>
2021-06-14 16:06:31 -0400

    add explanation for no content

--- commands/misc/firstmsg.js
+++ commands/misc/firstmsg.js
@@ -7,12 +7,14 @@ module.exports =
     {
         const fetchMessages = await(message.channel.messages.fetch({after:1, limit:1}));
         const msg = fetchMessages.first();
+        let messageCont = msg.content;
+        messageCont ? messageCont = `"${msg.content}"` : messageCont = `"${msg.content}" (this message has no content: it is either a welcome message or has an attachment)`;
         let embed =
         {
             color: global.blueCol, title: `the first message in this channel`, url:msg.url, thumbnail:{url:msg.author.displayAvatarURL({dynamic:true, size:4096})},
             description: `the first in ${message.channel}`, fields:
             [
-                {name: 'content', value: `"${msg.content}"`}, {name: 'author', value: `${msg.author} (${message.author.id})`}, {name: 'date', value: `${dateFormat(msg.createdTimestamp, 'default', true)} UTC`}
+                {name: 'content', value: messageCont}, {name: 'author', value: `${msg.author} (${message.author.id})`}, {name: 'date', value: `${dateFormat(msg.createdTimestamp, 'default', true)} UTC`}
             ], footer: global.footer
         };
         return message.channel.send({embed:embed});

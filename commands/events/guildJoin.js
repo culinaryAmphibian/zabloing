f69551a24474097d3b1df2cb323fe974d9b068c7
culinaryAmphibian <teperbudget@gmail.com>
2021-06-15 08:33:00 -0400

    minor fixes to embed

--- commands/events/guildJoin.js
+++ commands/events/guildJoin.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const ServersJSON = require('../../DB/servers.json');
 
-let embed = {color: global.blueCol, title: '', description: '', fields: [], footer: global.footer};
+let embed = {color: global.blueCol, title: '', description: '', fields: [], footer: {text: '', icon_url: ''}};
 
 module.exports =
 {
@@ -10,7 +10,8 @@ module.exports =
     {
         let channel = guild.channels.cache.find(c => c.name.includes('general')) || guild.channels.cache.random();
         if (!channel) return;
-        embed.footer.icon_url = guild.iconURL({dynamic: true});
+        embed.footer.text = guild.name;
+        embed.footer.icon_url = guild.iconURL({dynamic: true}) || bot.user.displayAvatarURL({dynamic: true});
         if (!ServersJSON[guild.id])
         {
             await bot.commandsForInternalProcesses.get('newServer').execute(guild);
@@ -18,7 +19,7 @@ module.exports =
             embed.description = 'nice to meet you';
             embed.fields.push({ name: 'prefix', value: 'my prefix by default is "."'},
                 {name: 'you can change my prefix with the setprefix command', value: 'just prefix it with the new prefix.'},
-                {name: 'my commands', description: 'type `.help` for a list of commands'});
+                {name: 'my commands', value: 'type `.help` for a list of commands'});
             return channel.send({embed:embed});
         }
         ServersJSON[guild.id].log.push({type: 'join', time: new Date().getTime()});
@@ -27,7 +28,7 @@ module.exports =
         let prefix = ServersJSON[guild.id].prefix || '.';
         embed.title = `hello, i\'m ${bot.user.username}`;
         embed.description = 'something tells me that i\'ve been here before...';
-        embed.fields.push({name: `my`, value: `you can see all my commands through the help command`},
+        embed.fields.push({name: `my commands: `, value: `you can see all my commands through the help command`},
         {name: `my current prefix is ${prefix}`, value: 'you can change it by running <preferred prefix>setprefix'});
         channel.send({embed: embed});
         return fs.writeFileSync('./DB/servers.json', JSON.stringify(ServersJSON, null, 2));
